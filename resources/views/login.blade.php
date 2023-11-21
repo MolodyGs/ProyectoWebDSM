b5984873d58918ade5321250a7d75a8aa5606d70
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2023-11-21 20:47:45 -0300

    Cambio de estilos, blur

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,8 +9,43 @@
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0 "></script>
     @vite('resources/css/app.css')
 </head>
-<body class="body font">
-    <section>
+<body class="body">
+    <section class="font container">
+        <div class="containerbackground">
+            <p>import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*; // needed for event handling
+
+public class Snake
+  
+  static final int SCREEN_SIZE_X=40;         // In units of snake sections.
+  static final int SCREEN_SIZE_Y=30;
+  
+  final int MAX_SNAKE_LENGTH = 1000;
+  
+  // While a snake is created with a very large number of snake sections,
+  // determined by the constant variable MAX_SNAKE_LENGTH, the actual
+  // apparent length of the snake in the game will be controlled by the
+  // variable snakeLength. At each time step, the program will only draw
+  // snakeLength snake sections for each snake. 
+  //
+  // Each time the snake eats some "food" (a yellow box on the screen), 
+  // the snakeLength for that snake will
+  // grow by the value of the food, which is printed in the box representing the
+  // food. 
+  
+  int snakeLength = 5;                      // Start snakes with length 5.
+  SnakeSection [] snakeSecs = new SnakeSection[MAX_SNAKE_LENGTH];
+   
+  // These variables represent the direction the snake is going.
+
+    </p>
+        </div>
+        <div>
+            <h1 class="center socialDev">
+                Social_Dev
+            </h1>
+        </div>
         <div class="formDiv">
             <div class="">
                 <form class="form" action="{{ route('loginAuth')}}" method="POST">

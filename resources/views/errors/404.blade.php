2a196cc537c9a4ae3d593c39510f04c3c09062d9
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2023-11-29 23:07:14 -0300

    fix: Texto para página 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404</title>
     @vite('resources/css/login.css')
+    @vite('resources/css/keyframes.css')
 </head>
 <body class="body">
     <h1 class="center title">Error 404 :c</h1>
